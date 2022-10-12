00a24b0b594cbf40dc5a685e84b5e6235c2db407
unknown <selmarionx@gmail.com>
2022-10-12 14:35:26 +0300

    Task1done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,4 +4,23 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+Console.Clear();
 
+Console.WriteLine("Введите первое число: ");
+int numbA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numbB = int.Parse(Console.ReadLine());
+
+int exponentiation = Exponentiation(numbA, numbB);
+Console.WriteLine($"Натуральная степень первого числа от второго = {Exponentiation(numbA, numbB)}");
+
+int Exponentiation(int numbA, int numbB)
+{
+    int result = 1;
+    for (int i = 1; i <= numbB; i++)
+    {
+        result = result * numbA;
+    }
+    return result;
+}
